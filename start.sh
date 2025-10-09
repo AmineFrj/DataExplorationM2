1be0650699c0d549123ff73da600f5af60f4cede
Amine Ferdjaoui <50391289+AmineFrj@users.noreply.github.com>
2025-10-09 12:32:19 +0200

    Update start.sh
    Le serveur n'existe pas ca pourrait bloquer le lancement du container

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=1234567890 --NotebookApp.password=1234567890 &
-uvicorn scripts.serve:app --host 0.0.0.0 --port 8000
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=1234567890 --NotebookApp.password=1234567890 
